f3e146e36e1945a74df3558c3773c0277bb22783
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-17 11:43:36 +0530

    add : Program.cs file
    which contains a entry point of project

--- CSharp_PR_8/Program.cs
+++ CSharp_PR_8/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace CSharp_PR_8
+﻿using CSharp_PR_8.Pages;
+using LibraryManagement;
+using LibraryManagement.Interface;
+
+namespace CSharp_PR_8
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			GeneralPage.RunGeneralPage();
 		}
 	}
 }
