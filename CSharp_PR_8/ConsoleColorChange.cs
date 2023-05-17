99eb37aaf2c24a846c9b01e519261bdc8bff7bf7
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-17 11:42:23 +0530

    add : consolecolorchanges class
    this class contains a method for chnaging the color of console.
    here in project i used a two colors blue and green
    blue is for input and green for messages

---
+++ CSharp_PR_8/ConsoleColorChange.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp_PR_8
+{
+    internal static class ConsoleColorChange
+    {
+
+        public static void MakeColorBlue()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+
+        public static void MakeColorGreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
